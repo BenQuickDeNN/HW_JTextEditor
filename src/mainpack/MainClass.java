0926cef42bb80c26215d810ed947a522c1fc7e26
BenQuickDeNN <2413269761@qq.com>
2017-11-12 10:49:42 +0800

    初始化

---
+++ src/mainpack/MainClass.java
@@ -0,0 +1,19 @@
+package mainpack;
+
+/**
+ * Java 文本编辑器 主类
+ * @author Ben Quick in XJTU
+ *
+ */
+public class MainClass {
+
+	public MainClass() {
+		
+	}
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
